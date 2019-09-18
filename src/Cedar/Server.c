6c426aecd719ba96e99b191ef75feb13f1679f50
Koichiro IWAO <meta@vmeta.jp>
2019-09-18 10:12:00 +0900

    unrestrict enterprise functions

--- src/Cedar/Server.c
+++ src/Cedar/Server.c
@@ -10899,23 +10899,7 @@ bool SiCheckCurrentRegion(CEDAR *c, char *r)
 // 
 bool SiIsEnterpriseFunctionsRestrictedOnOpenSource(CEDAR *c)
 {
-	char region[128];
-	bool ret = false;
-	// Validate arguments
-	if (c == NULL)
-	{
-		return false;
-	}
-
-
-	SiGetCurrentRegion(c, region, sizeof(region));
-
-	if (StrCmpi(region, "JP") == 0 || StrCmpi(region, "CN") == 0)
-	{
-		ret = true;
-	}
-
-	return ret;
+	return false;
 }
 
 // Update the current region
